e2297dccbeb5b90a59378e0db909005a6c5102ae
Joabesv <joabevarjao123@gmail.com>
2023-01-20 16:43:00 -0300

    feat(service): implement service

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,27 @@
+import { p } from '../../database/connection';
+import type { CreateProductInput } from './product.schema';
+
+export async function createProduct(
+  data: CreateProductInput & { ownerId: number }
+) {
+  return p.product.create({ data });
+}
+
+export async function getProducts() {
+  // i know its ugly as hell
+  return p.product.findMany({
+    select: {
+      content: true,
+      title: true,
+      price: true,
+      createdAt: true,
+      updatedAt: true,
+      owner: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+    },
+  });
+}
