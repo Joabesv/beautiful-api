bdd9d85fa3005953ee2abdaff2f92c071f15a261
Joabesv <joabevarjao123@gmail.com>
2023-01-22 00:37:49 -0300

    feat(tests): start writing tests

---
+++ tests/app.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from 'vitest';
+import { buildServer } from '../src/server';
+
+
+describe('App tests' ,() => {
+  test('request the healthCheck', async () => {
+    const fastify = await buildServer();
+    const response = await fastify.inject({
+      method: 'GET',
+      url: '/healthCheck'
+    })
+    
+    expect(response.statusCode).toEqual(200)
+    expect(response.json()).toEqual({ "status": "OK" })
+  });
+})
