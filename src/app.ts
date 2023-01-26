2763282bdacc77e87aa63d80ec8733cbd850d626
Joabesv <joabevarjao123@gmail.com>
2023-01-25 23:15:18 -0300

    test(database): make all tests run fixing db schema

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ async function server() {
   const app = await buildServer();
   try {
     await app.listen({ port: 5000, host: '0.0.0.0' });
-    app.log.info('server ready at http://localhost:5000');
+    app.log.info(app.printRoutes({ commonPrefix: false }), 'routes');
   } catch (e) {
     app.log.error(e, 'Error in server startup');
     process.exit(1);
